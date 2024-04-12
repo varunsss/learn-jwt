ea7f8c4a6e762760cd84c798dd833c679badfbb9
varunsss <varunsonkar03@gmail.com>
2024-04-12 15:09:53 +0530

    Add files via upload

---
+++ JWTSBproject1/src/main/java/com/varun/spring/security/jwt/JwtsBproject1Application.java
@@ -0,0 +1,13 @@
+package com.varun.spring.security.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtsBproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtsBproject1Application.class, args);
+	}
+
+}
